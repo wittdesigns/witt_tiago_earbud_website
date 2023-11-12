41cfbf0c16b699fcfe25449346f405388b1372df
wittdesigns <t_dasilvawitt@fanshaweonline.ca>
2023-11-11 22:54:23 -0500

    Fixing AR version
    Fixed AR model, changes affect hotspots, gsap animation p, and image (easy in)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 (() => {
 
+    gsap.fromTo('#battery_info', {
+        y: 35,
+        opacity: 0,
+      },
+      {
+      delay: 1, 
+      duration: 3, 
+      y: 4,
+      opacity: 1,
+      ease: 'power2.easeOut',
+      stagger: {
+        from: 'start', 
+        amount: 1.5, 
+      },
+    })
+
+
+
 })();
